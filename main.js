cdb4c4799e2fb2b895868e3eb12e6bd55e7d626a
David Robyn <David@Ds-MacBook.local>
2018-02-16 13:05:45 -0800

    Starting the Longscroll

---
+++ main.js
@@ -0,0 +1,30 @@
+//document.getElementById('counter').innerHTML = "hello";
+
+//initalize variables - set-up variables
+var counter = document.getElementById('counter');
+var fromTop = window.pageYOffset;
+
+counter.innerHTML = "counter text";
+
+//Pseudocode when the user is scrolling update the variable fromTop and make decisions to change stuff on the page
+//window.onscroll is a boolean (yes/no)
+window.onscroll = function () {
+    //when the user scrolls, do all of this!
+
+    fromTop = window.pageYOffset;
+    //test if it works
+    console.log(fromTop);
+
+    counter.style.top = 100 + fromTop + "px";
+
+    if(fromTop > 1000){
+      counter.innerHTML = "More than 1000";
+      counter.style.backgroundColor="red";
+      counter.style.backgroundImage="url(yellowsubmarine.png)";
+    }
+    else {
+      counter.innerHTML = "counter text";
+      counter.style.backgroundColor="green";
+    }
+
+}
